ba44a86ec2f5aa7810b49f5dd9980dc5df84e02a
loginovskiy <vv.loginovskiy@gmail.com>
2018-11-07 00:43:21 +0300

    Класс сортировок продолжение

--- src/ru/loginovskiy/algorithm/sorts/Sorts.java
+++ src/ru/loginovskiy/algorithm/sorts/Sorts.java
@@ -1,8 +1,12 @@
 package ru.loginovskiy.algorithm.sorts;
 
+import java.util.Arrays;
+
 public class Sorts
 {
-    int[] a;
+    private int[] a;
+    private int[] heap;
+    private int heapSize;
 
     public void bubleSort(int[] arr)
     {
@@ -36,21 +40,88 @@ public class Sorts
         a = arr;
         for(int i = 0; i<a.length - 1; i++)
         {
-            for(int j=i+1; j > 0; j--)
+            for(int j=i+1; j > 0 && a[j-1] > a[j]; j--)
             {
-                if(a[j-1] > a[j])swap(j-1,j);
-                else break;
+                swap(j-1,j);
             }
+//            int j=i+1;
+//            while(j>0 && a[j-1] > a[j])swap(j-1,j--);
         }
     }
 
     public void heapSort(int[] arr)
     {
+        a = arr;
+        heap = new int[a.length];
+        heapSize = 0;
+        for (int i = 0; i < a.length; i++)
+        {
+            insert(a[i]);
+        }
+        System.out.println(Arrays.toString(heap));
+        for (int i = 0; i < a.length; i++)
+        {
+            a[i]=extract();
+        }
+    }
+    private void insert(int val)
+    {
+        heapSize++;
+        heap[heapSize-1] = val;
+        shiftUp();
+    }
 
+    private void shiftUp()
+    {
+        if(heapSize-1 == 0)return;
+        int i = heapSize - 1;
+        while(i>0 && heap[i]<heap[(i-1)/2])
+        {
+            swaph(i,(i-1)/2);
+            i = (i-1)/2;
+        }
+    }
+
+    private int extract()
+    {
+       int temp = heap[0];
+       heap[0] = heap[heapSize - 1];
+       shiftDown();
+       return temp;
+    }
+
+    private void shiftDown()
+    {
+        int left,right, i, j;
+        i=0;
+        while(2*i+1<heapSize)
+        {
+            left = 2 * i + 1;
+            right = 2 * i + 2;
+            j = left;
+            if (right < heapSize && heap[right] < heap[left])
+            {
+                j = right;
+            }
+            if(heap[j] < heap[i])swaph(i,j);
+            i=j;
+        }
     }
 
     public void mergeSort(int[] arr)
     {
+        a = arr;
+        int right, left, i;
+        right = 0;
+        left = a.length - 1;
+        sort(right, left);
+    }
+
+    private void sort(int r, int l)
+    {
+        int right = r;
+        int left = l;
+        int midle = (right + left)/2;
 
     }
 
@@ -64,5 +135,12 @@ public class Sorts
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
+    }
+
+    private void swaph(int i, int j)
+    {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
     }
 }
